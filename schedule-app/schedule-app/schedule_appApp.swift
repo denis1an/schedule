d1e6faa6a7360ded20701c5b15746b2fa079fbab
Denis <denis.andreevir@gmail.com>
2020-09-20 19:29:15 +0300

    init app

---
+++ schedule-app/schedule-app/schedule_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  schedule_appApp.swift
+//  schedule-app
+//
+//  Created by Denis Andreev on 20.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct schedule_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- src/main/java/ru/andreev/lectureschedule/controller/AdminController.java
+++
@@ -1,8 +0,0 @@
-package ru.andreev.lectureschedule.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class AdminController {
-
-}
